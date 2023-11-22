8e89021c9682d071240af63c90c2b56962513af5
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-11-22 19:36:25 -0300

    Adds more title text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Smart Peso</h1>
+      <h1>Smart Peso - Personal Finance</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
