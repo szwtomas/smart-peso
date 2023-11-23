8e144bd085a1148a93f713bcca2cfbf4db09220a
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-11-23 00:29:49 -0300

    Adds router and some configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Providers } from "./Providers";
 import { Layout } from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <div>Hello world!</div>
+      </Layout>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <Layout>
+        <div>Dashboard</div>
+      </Layout>
+    ),
+  },
+  {
+    path: "/transactions",
+    element: (
+      <Layout>
+        <div>Transactions</div>
+      </Layout>
+    ),
+  },
+  {
+    path: "/configuration",
+    element: (
+      <Layout>
+        <div>Configuration</div>
+      </Layout>
+    ),
+  },
+]);
 
 const App = () => {
   return (
     <>
       <Providers>
-        <Layout>
-          <h1>Holis</h1>
-        </Layout>
+        <RouterProvider router={router} />
       </Providers>
     </>
   );
--- frontend/src/components/sidebar/SidebarLinks.tsx
+++ frontend/src/components/sidebar/SidebarLinks.tsx
@@ -3,22 +3,25 @@ import {
   HomeIcon,
   BanknotesIcon,
 } from "@heroicons/react/24/outline";
+import { useLocation } from "react-router-dom";
+import { clsx } from "clsx";
 
 const links = [
   { name: "Dashboard", href: "/dashboard", icon: HomeIcon },
   {
     name: "Transactions",
-    href: "/dashboard/transactions",
+    href: "/transactions",
     icon: BanknotesIcon,
   },
   {
     name: "Configuration",
-    href: "/dashboard/configuration",
+    href: "/configuration",
     icon: Cog8ToothIcon,
   },
 ];
 
 export function SidebarLinks() {
+  const { pathname } = useLocation();
   return (
     <>
       {links.map((link) => {
@@ -27,9 +30,12 @@ export function SidebarLinks() {
           <a
             key={link.name}
             href={link.href}
-            className={
-              "flex flex-row h-[48px] items-center justify-around gap-2 rounded-md p-1 text-xl font-medium hover:bg-sky-100 hover:text-primary md:flex-none md:justify-start md:p-2 md:px-3"
-            }
+            className={clsx(
+              "flex flex-row h-[48px] items-center justify-around gap-2 rounded-md p-1 text-xl font-medium hover:bg-sky-100 hover:text-primary md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-primary": pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <span className="hidden md:block">{link.name}</span>
