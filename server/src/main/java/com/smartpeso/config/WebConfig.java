7b163bc36b864a0c7fdcefeb1708e98ee7264b08
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-12-10 15:34:19 -0300

    Now CORS allows smartpeso prod domain

--- server/src/main/java/com/smartpeso/config/WebConfig.java
+++ server/src/main/java/com/smartpeso/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://www.smartpeso.com.ar")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
