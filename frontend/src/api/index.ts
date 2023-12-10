8a3af78cdae7725a4b568a91993dd5b5a80c6d11
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-12-10 15:22:06 -0300

    Frontend using backend host

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,15 @@
 import { Api } from "./api";
 
-export const api = new Api(import.meta.env.VITE_BACKEND_HOST as string);
+function getHost(): string {
+    if (process.env.VITE_VERCEL_ENVIRONMENT === "prod") {
+        const prodHost = process.env.VITE_VERCEL_PROD_HOST as string;
+        console.log("Setting host: ", prodHost);
+        return prodHost;
+    } else {
+        const localhost = import.meta.env.VITE_BACKEND_HOST as string;
+        console.log("Setting host: ", localhost);
+        return localhost;
+    }
+}
+
+export const api = new Api(getHost());
