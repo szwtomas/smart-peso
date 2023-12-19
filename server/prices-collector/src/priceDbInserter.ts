6084341063b9034ab88770a7f43ec39f27dc69f3
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-12-19 15:24:13 -0300

    Cron does not stop on error

--- server/prices-collector/src/priceDbInserter.ts
+++ server/prices-collector/src/priceDbInserter.ts
@@ -6,24 +6,22 @@ export function insertPricesInDB(prices: PriceCollectorResult) {
     const connection = mysql.createConnection(connectionOptions);
     connection.connect((connectionError) => {
         if (connectionError) throw connectionError;
-
         console.log("Connected to database");
         const sql = 'INSERT INTO currencyPrices SET ?';
-
         const rowData = {
             usdOfficial: prices.usdOfficial,
             usdBlue: prices.usdBlue,
             usdCCL: prices.usdCCL,
             usdMEP: prices.usdMEP,
-        }
+        };
         
         connection.query(sql, rowData, (queryError, results: ResultSetHeader) => {
             connection.end();
-
             if (queryError || !results || results.affectedRows !== 1) {
                 console.error("Error inserting price: " + queryError?.message);
-                process.exit(1);
+                return;
             }
+
             console.log(`Inserted ${results.affectedRows} row(s) in database`);
             console.log("Prices inserted: " + JSON.stringify(rowData));
         });
